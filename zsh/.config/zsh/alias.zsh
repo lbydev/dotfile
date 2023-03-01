ed1737a937f7aa15c751a90427722fbbd68c7989
lbydev <lbydev>
2023-03-01 11:20:12 +0800

    add wechat alias

--- zsh/.config/zsh/alias.zsh
+++ zsh/.config/zsh/alias.zsh
@@ -58,3 +58,6 @@ alias j='z'
 
 # macos only
 alias refresh-dns='sudo killall -HUP mDNSResponder'
+
+# wechat 
+alias wechat='nohup /Applications/WeChat.app/Contents/MacOS/WeChat > /dev/null 2>&1 &'
